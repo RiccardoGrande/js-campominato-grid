0bd1fbd5d02114f2ff31ccbf014c145144629b8a
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-17 16:18:40 +0100

    add structure for start grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,10 @@
 
 const container = document.querySelector(".container");
 
+//creo una variabile dove salvo l'input del bottone da premere
+
+const buttonStart = document.querySelector(".button_start");
+
 // creo un max numero di celle
 
 let nMax = 100;//Ci saranno quindi 10 caselle per ognuna delle 10 righe.
@@ -18,3 +22,23 @@ for (let i = 0; i < nMax; i++) {
     const cell = `<div class="cell"></div>`;
     container.innerHTML += cell;
   }
+
+  // seleziono una cella che ha classe cell e active
+
+const cellEl = document.querySelectorAll(".cell")
+
+// aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
+
+
+for (let i = 0; i < cellEl.length; i++) {
+    const thisCell = cellEl[i];
+    console.log(thisCell)
+    thisCell.addEventListener("click", function() {
+        thisCell.classList.toggle("bg_lightblue")
+        // this.classList.add("active")
+        console.log("You clicked number ${}")
+    })
+
+
+    
+}
