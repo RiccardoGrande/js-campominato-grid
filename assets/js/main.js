f0cc1c7933a657273301ccb2727c94336ba893f6
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-20 10:53:23 +0100

    add fix innerHTML code

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ let nMax = 100;//Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 
 
 //creo una variabile che mi svuota il container prima di aver premuto il bottone
-container.innerHtml = ``;
+container.innerHTML = ``;
 
 //successivamente creo un evento che funge da 'click' del bottone che fa apparire la griglia di gioco
 // creo un ciclo n volte per le celle (dove n in questo caso sta per 100) all'interno dell'evento
@@ -27,9 +27,14 @@ container.innerHtml = ``;
 
 buttonStart.addEventListener("click", function(){
    
+    container.innerHTML = ``;
     for (let i = 1; i <= nMax; i++) {
+        
         const cell = `<div class="cell">${i}</div>`;
+
+        
         container.innerHTML += cell;
+        
     }
 
     
@@ -38,19 +43,21 @@ buttonStart.addEventListener("click", function(){
   // seleziono una cella che ha classe cell e active
 
     const cellEl = document.querySelectorAll(".cell")
-
+    
     // aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
 
 
     
 
     for (let j = 0; j < cellEl.length; j++) {
+        
         const thisCell = cellEl[j];
         console.log(thisCell)
         thisCell.addEventListener("click", function() {
             thisCell.classList.toggle("bg_lightblue")
             // this.classList.add("active")
             console.log(`You clicked number ${j + 1}`)
+            
         })
     };
 
